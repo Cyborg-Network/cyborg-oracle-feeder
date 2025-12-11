b7f7ce9fc1d9d670eab0ef6e23ac96f1694b2304
Tom Bleek <tom.bleek@proton.me>
2025-12-11 17:15:54 +0100

    Adapt miner health check url

--- src/feeder.rs
+++ src/feeder.rs
@@ -239,7 +239,7 @@ impl OracleFeeder for CyborgOracleFeeder {
 
         let client = Client::new();
         let response = client
-            .get(format!("{}/agent-health/check-health", miner_ip))
+            .get(format!("{}/check-health", miner_ip))
             .timeout(Duration::from_secs(5))
             .send()
             .await;
